083106be7b45a1863c0f32da9631ac56dc629d85
amisha26 <amisha8299@gmail.com>
2023-10-18 13:10:58 -0400

    assignment 3 done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-//import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Labs from "./Labs/a3";
+import Kanbas from "./Kanbas";
+import HelloWorld from "./Labs/a3/HelloWorld";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1>Helloooo!</h1>
+      <Routes>
+        <Route path="/" element={<SelectOptions />} />
+        <Route path="/Labs/*" element={<Labs />} />
+        <Route path="/hello" element={<HelloWorld />} />
+        <Route path="/kanbas/*" element={<Kanbas />} />
+      </Routes>
+    </div>
+  );
+}
+
+function SelectOptions() {
+  return (
+    <div style={{ display: "flex", gap: "10px" }}>
+      <button className="btn btn-primary">
+        <Link to="/Labs" style={{ textDecoration: "none", color: "#fff" }}>
+          Labs
+        </Link>
+      </button>
+
+      <button className="btn btn-primary">
+        <Link to="/kanbas" style={{ textDecoration: "none", color: "#fff" }}>
+          Kanbas
+        </Link>
+      </button>
     </div>
   );
 }
