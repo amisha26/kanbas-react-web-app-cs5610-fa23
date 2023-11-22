b34f963ed32392f110296ff3f72fb2ebba7a27f5
amisha26 <amisha8299@gmail.com>
2023-11-22 17:46:58 -0500

    commented unused imports

--- src/Labs/index.js
+++ src/Labs/index.js
@@ -1,10 +1,10 @@
 //import Nav from "../Nav";
 //import Assignment3 from "./a3";
-import Assignment4 from "./a4";
-import AddRedux from "./a4/ReduxExamples/AddRedux";
-import CounterRedux from "./a4/ReduxExamples/CounterRedux";
-import HelloRedux from "./a4/ReduxExamples/HelloRedux";
-import TodoList from "./a4/ReduxExamples/todos/TodoList";
+// import Assignment4 from "./a4";
+// import AddRedux from "./a4/ReduxExamples/AddRedux";
+// import CounterRedux from "./a4/ReduxExamples/CounterRedux";
+// import HelloRedux from "./a4/ReduxExamples/HelloRedux";
+// import TodoList from "./a4/ReduxExamples/todos/TodoList";
 //import { Routes, Route, Navigate } from "react-router";
 import store from "./store/index";
 import { Provider } from "react-redux";
